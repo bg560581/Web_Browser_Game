e444bf1f3564b5aacc6dd40b1d2d87999e027c08
bg560581 <bennett.galgano@gmail.com>
2022-12-17 00:48:22 -0500

    creating motion on object and figuring out how to get it to move constantly

--- main.js
+++ main.js
@@ -1,93 +1,171 @@
 // //create game area
 
+let canvas = document.getElementById("gameArea");
+    let ctx = canvas.getContext("2d");
+    let x = canvas.width - 500;
+    let y = canvas.height - 25;
 
-function startGame() {
-    arena.start();
-}
-const arena = document.getElementById('gameArea')
-const ctx = arena.getContext('2d')
-const player = new Image();
-player.src = 'tree.png'
 
-class picture {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    drawCharacter(player) {
-        player.onload = function() {
-            ctx.drawImage(player, 15,130);
-            player.style.width = '50px'
-            console.log(player)
-        };
-    }
-    drawEnemy(img){
-        img.onload = function(){
-            ctx.drawImage(img, this.x, this.y)
+
+    function drawPlayer() {
+        ctx.beginPath();
+        ctx.arc(x, y, 15, 0, Math.PI*2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+        ctx.x = 0;
+    }  
+ const id = setInterval(()=> {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawPlayer()
+        x += 1
+
+        if (x > canvas.width + 11){
+            clearInterval(id)
         }
-    }
+        if (clearInterval() === 'true'){
+        setInterval(id, 10)
+        }
+    }, 20);
+
+
+//figure out a way to get the draw function in the re
+
+
+let start = 0;
+
+function animate(timestamp) { 
+  const elapsed = timestamp - start;
+  if (elapsed > 200) {
+    start = timestamp;
+    tick();
+  }
+  requestAnimationFrame(animate); 
+}
+let started = false
+
+//need to pass id variable in animate function in order to get constant motion
+if (!started){
+    animate();
+}
+function animate(){
+    draw();
+    requestAnimationFrame(animate)
 }
 
-let objectsImg = ['boulder.png', 'sword.png', 'staff.png']
-for (let i = 0; i < objectsImg.length; i++){
-        const img = new Image();
-            img.src = objectsImg[i]
-            let x = getRandomInt(0,500);
-            let y = getRandomInt(0,300);
-            // let x = getRandonArbitrary(0, 500)
-            // let y = getRandonArbitrary(0, 300)
-            console.log(x)
-            console.log(y)
+
+
+    // function drawPlayer2() {
+    //     ctx.fillStyle = "#FF0000";
+    //     ctx.fillRect(x, 10, 50, 50);
+    // }
+    // function draw() {
+    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //     drawPlayer();
+    //     // drawPlayer2();
+    //     x -= dx;
+    // }
+    // function draw2(){
+    //     ctx.clearRect(0,0, canvas.width, canvas.height)
+    //     drawPlayer2();
+    //     x += dx;
+    // }
+    // setInterval(draw2, .5)
+    // draw()
+    // setInterval(draw, 10)
+    // draw2()
+
+
+      
+    
+
+
+
+
+    // if (x = 500){
+    //         path == true
+    // }
+
+
+    // while (true){
+    // setInterval(draw, 0.25)
+// } 
+    // setInterval(draw, 0.25)
+
+    
+    // var motion = true
+    //     setInterval(
+    //         function (){
+    //             console.log(motion)
+    //         if (motion){
+    //             draw
+    //         }else{
+    //             return
+    //         }                
+    //          .05)
+        // setTimeout(
+        //     setInterval(draw2, .05))
+    
+    // const tree = new Image();
+    // tree.src = 'tree.png'
+    // tree.onload = function(){
+    //     ctx.drawImage(tree, 50, 100)
+    // }
+    
+    
+        
+    // class picture {
+    //     constructor(x, y, dx, dy) {
+    //         this.x = x;
+    //         this.y = y;
+    //         this.dx = dx
+    //         this.dy = dy
+    //     }
+    //     drawCharacter(player) {
+    //         player.onload = function() {
+    //             ctx.drawImage(player, 50, 100);
+    //             console.log(player)
+    //         };
+    //     }
+    //     drawEnemy(enemy){
+    //         enemy.onload = function(){
+    //             ctx.drawImage(enemy, 300, 175)
+    //             console.log(enemy)
+    //         };
+    //     }
+    
+    // }
+    
+
+// function startGame() {
+//     arena.start();
+// }
+
+
+
+// var pic = new picture('','')
+// pic.drawCharacter(player)
+
+
+// let objectsImg = ['boulder.png', 'sword.png', 'staff.png']
+// for (let i = 0; i < objectsImg.length; i++){
+//         const rock = new Image();
+//         rock.src = objectsImg[i]
+//             let x = getRandomInt(0,500);
+//             let y = getRandomInt(0,300);
+//             console.log(x)
+//             console.log(y)
             
-
-        const pic = new picture(x, y)
-            pic.drawCharacter(player)
-
-        const enemy = new picture(x,y)
-            enemy.drawEnemy(img)
+//             const enemy = new picture(x,y)
+//             enemy.drawEnemy(rock)
             
-}
+//         }
+        
+        
+//         function getRandomInt(min, max){
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min) + min);
+// }
 
-
-function getRandomInt(min, max){
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-
-// const arena = document.getElementById('gameArea')
-// const ctx = arena.getContext('2d')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // create image and have it placed middle-left side of frame
-//         //start with image in top left corner
-// //  function draw() {
-// //           const canvas = document.getElementById("box");
-// //           if (canvas.getContext) {
-// //             const ctx = canvas.getContext("2d");            
-// //           }
-// // }  
-
-// //first start with gravity of player
-//     // create movement of player
-//         //use button to make image jump
-//         //use gravity to drop image 
-
-// //make another image that that will eventually move from right side of box to left 
+// drawPlayer()
