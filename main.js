44599d2a5eb3884165e2554e32211ae03f0655b1
bg560581 <bennett.galgano@gmail.com>
2022-12-21 00:47:50 -0500

    obst moving, goal moving, score

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
 let canvas;
 let context;
+const button = document.getElementById('button')
 
 window.onload = init;
-//init() = false;
-//window.onload = button
-//let button = createElement('button')
-//button.textContent = 'Start Game'
-//button.addEventListener('click', function(){
-//   init() == true
-//   startGameHide(button)
-});
+// window.onload = button
+// button.textContent = 'Start Game'
+// button.addEventListener('click', function(){
+    // init()
+    // startGameHide(button)
+// });
 function init(){
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
     createWorld()
-
+    
     window.requestAnimationFrame(gameLoop);
 }
 
-//function startGameHide(button){
-//    if (init() == false){
-//button.style.display = 'block'}
-// else {button.style.display == 'none'}
+function startGameHide(button){
+   if (init() == false){
+button.style.display = 'block'}
+else {button.style.display == 'none'}
 }
 
 const canvasWidth = 750;
@@ -52,6 +51,8 @@ class Square extends GameCharacters {
     draw(){
         this.context.fillStyle = this.isColliding?'#ff8080':'#0099b0';
         this.context.fillRect(this.x, this.y, this.width, this.height);
+        drawScore()
+        
 
     }
 
@@ -71,17 +72,21 @@ let player1;
 let obs1;
 let obs2;
 let obs3;
+let score = 0
 function createWorld(){
     ball = new Square(context, 5, 325, 0, 0, 25, 30);
-    goal = new Square(context, 725, 325, 0, 0, 75, 75);
+    goal = new Square(context, 725, 325, 0, -250, 75, 75);
     goal.goal = true
+    goal.status === 1
     player1 = new Square(context, 5, 325, 0, 0, 25, 30);
     player1.player1 = true
-    obs1 = new Square(context, 250, 150, 50, 50, 25, 25);
-    obs2 = new Square(context, 350, 75, -50, 50, 25, 25);
-    obs3 = new Square(context, 300, 350, 50, -50, 25, 25);
+    obs1 = new Square(context, 250, 150, 200, -50, 25, 25);
+    obs2 = new Square(context, 350, 75, 200, 150, 25, 25);
+    obs3 = new Square(context, 300, 350, -50, -150, 25, 25);
     gameObjects = [ball, goal, player1, obs1, obs2, obs3];
+    obstacles = [obs1, obs2, obs3]
     console.log(gameObjects)
+    console.log(obstacles)
     document.addEventListener("keydown", function(e){
       console.log(player1.y)
         movePlayer(e)
@@ -101,7 +106,9 @@ function gameLoop(timeStamp){
     for (let i = 0; i < gameObjects.length; i++){
         gameObjects[i].update(secondsPassed)
     }
-    
+    for (let i = 0; i < obstacles.length; i++){
+        obstacles[i].update(secondsPassed)
+    }
     let gameResult = detectCollision()
     context.clearRect(0, 0, canvas.width, canvas.height); 
     
@@ -109,13 +116,30 @@ function gameLoop(timeStamp){
         ball.y = player1.y;
         ball.x = player1.x;
         ball.vx = 0;
-
     }
+
+    if (goal.y + goal.vy > canvasHeight + 175 || goal.y + goal.vy < goal.height - 325){
+        goal.vy = - goal.vy
+    }
+
+
+    for (let i = 0; i < obstacles.length; i++){
+        const obst = obstacles[i]
+        
+        if (obst.x + obst.vx > canvasWidth + 175 || obst.x + obst.vx < obst.width - 220 || obst.y + obst.vy > canvasHeight + 125 || obst.y + obst.vy < obst.height - 175){
+            obst.vx = -obst.vx
+            obst.vy = -obst.vy
+        }
+    }
+    
+    
     for (let i = 0; i < gameObjects.length; i++){
         gameObjects[i].draw();
     }
+    for (let i = 0; i < obstacles.length; i++){
+        obstacles[i].draw();
+    }
     
-
     if (gameResult == false){
         window.requestAnimationFrame(gameLoop)
     } else {
@@ -142,13 +166,19 @@ let hitGoal = false
                 if (obj2.goal == true){
                     hitGoal = true
                     alert("goal!")
+                    score++
                 } else if(obj2.player1 == false){
                     alert("YOU LOSE!")
                 }                
             }
         }
+        if (hitGoal == true){
+        }
         return hitGoal
+    
     }
+
+
 
 function rectIntersect(x1, y1, w1, h1, x2, y2, w2, h2) {
     // Check x and y for overlap
@@ -156,6 +186,12 @@ function rectIntersect(x1, y1, w1, h1, x2, y2, w2, h2) {
         return false;
     }
     return true;
+}
+
+function drawScore(){
+  context.font = "16px Arial";
+  context.fillStyle = "#0095DD";
+  context.fillText("Score: "+score, 8, 20) 
 }
 
 
